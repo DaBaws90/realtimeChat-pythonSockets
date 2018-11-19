a3c4ea53cae54953f007b3456e1b6c4d6c7f47f0
DaBaws90 <pere_dki@hotmail.com>
2018-11-19 21:38:16 +0000

    Replace cliente.py

--- cliente.py
+++ cliente.py
@@ -9,7 +9,10 @@ except:
     sys.exit()
 
 # Esta función nos permite poner una cabecera a cada mensaje minetras enviamos o recibimos, indistintamente de la operación.
-# 
+# Nos sirve también para que en todo momento imprima el buffer que tiene almacenado, ya que de otra manera tendría que esperar
+# para hacerlo, puesto que sys.stdout va recolentando lo que se escribe antes de imprimirlo por el terminal.
+# De esta manera, cuando recibe e imprime un mensaje, se sigue mostrando nuestra cabecera, porque forzamos a que
+# toda esa información coexista sin tener que esperar a que la pantalla/terminal esté disponible.ore doing so. 
 def header():
     sys.stdout.write(">> ")
     sys.stdout.flush()
@@ -30,7 +33,11 @@ while True:
     # select(), así que tuve que buscar la manera de poder hacerlo en windows y este es el resultado. Básicamente
     # le damos una lista con las posibilidades que existen, o bien un mensaje del serivdor, o del usuario, y escucha dinamicamente
     # hasta que uno de los dos requiera interactuar. De esa manera, nuestro script no se verá encasillado en esperar para mandar
-    # o para escribir, si no que recibirá cuando pueda, y escribirá cuando quiera
+    # o para escribir, si no que recibirá cuando pueda, y escribirá cuando quiera.
+    # Tuve que indagar bastante hasta dar con la solución a este problema, y de no haberla encontrado, todavía seguiría
+    # sin poder hacer que evaluase dinámicamente dónde se producía la actividad, y de ese modo, pmostrar los mensajes
+    # recibidos o permitir que se pudiesen enviar, dependiendo de si la actividad se producía en el socket (desde el servidor) 
+    # o en el terminal (desde el cliente).
     
     for p in possibilities:
         # Por cada una de esas posibilidades, que compurebe cual está "activa" en cada momento. Si es un mensaje entrante,
@@ -48,6 +55,7 @@ while True:
         else:
 
             incoming = p.recv(1024).decode()
+            # Si no hay señal, puede que se deba a que la conexión se perdió,por loque enviamos un quit y desconectamos
             if not incoming:
                 s.send("quit".encode())
                 s.close()
